6b6fdf5526f69999eb7dc25735b07ef475a718a8
Sondre <fingann92@gmail.com>
2020-10-13 22:29:24 +0200

    initial commit

---
+++ src/jsonfuzzer/go-json.go
@@ -0,0 +1,9 @@
+package main
+
+// Todo is ...
+type Todo struct {
+	UserID    int    `json:"userId"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
